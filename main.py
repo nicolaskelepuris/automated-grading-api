39b9ca1f6609f1fc48958f053dc4d9aeeefd8918
nicolas <nicolas.kelepuris@gmail.com>
2022-09-16 23:08:31 -0300

    receive correct anwers param

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from controller import process
 import numpy as np
+import json
 
 app = FastAPI()
 filesList = []
@@ -15,8 +16,8 @@ app.add_middleware(
 )
 
 @app.post("/upload-file") 
-async def create_upload_file(files: list[UploadFile], choicesCount: int = Form(5)):
-  process(list(map(to_bytes, files)))
+async def create_upload_file(files: list[UploadFile], choicesCount: int = Form(5), correctAnswers: str = Form('[]')):
+  process(list(map(to_bytes, files)), correct_answers = json.loads(correctAnswers), choices_per_question_count = choicesCount)
   return { "uploadStatus" : "Complete" }
 
 def to_bytes(file: UploadFile):
