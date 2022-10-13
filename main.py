452a1643b95fcd777eb3364c2368aa4507fe5986
nicolas <nicolas.kelepuris@gmail.com>
2022-10-13 16:30:31 -0300

    add a root path

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
+@app.get("/")
+def root():
+  return "Running" 
+
 @app.post("/upload-file") 
 async def process_exams(files: list[UploadFile], choicesCount: int = Form(5), correctAnswers: str = Form('[]'), id_digits_count: int = Form(10)):
   return process(list(map(to_bytes, files)), correct_answers = json.loads(correctAnswers), choices_per_question_count = choicesCount, id_digits_count = id_digits_count)
